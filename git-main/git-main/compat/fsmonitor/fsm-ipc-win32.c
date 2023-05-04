c0f600193a89a2cc1b3aebba9edcaf4b7ac72a85
ipiwixx <124789918+ipiwixx@users.noreply.github.com>
2023-05-04 11:57:43 +0200

    fichier inutilie

--- git-main/git-main/compat/fsmonitor/fsm-ipc-win32.c
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-#include "fsmonitor-ipc.h"
-
-const char *fsmonitor_ipc__get_path(struct repository *r) {
-	static char *ret;
-	if (!ret)
-		ret = git_pathdup("fsmonitor--daemon.ipc");
-	return ret;
-}
--- test1.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-foreach($_SESSION as $key => $valeur)
-            {
-                echo 'key ='.$key.' valeur = '.$valeur;
-            } 
-?>
